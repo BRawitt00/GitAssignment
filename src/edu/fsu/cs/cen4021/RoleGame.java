13783b000d52e6663bc7db3fc56bd5555b5cc436
BRawitt00 <brawitt00@gmail.com>
2017-01-27 07:30:07 -0500

    Class SimpleArrow Added. Changes made to WeaponFactory and RoleGame accordingly.

--- src/edu/fsu/cs/cen4021/RoleGame.java
+++ src/edu/fsu/cs/cen4021/RoleGame.java
@@ -10,11 +10,18 @@ public class RoleGame {
 
     public static void main(String[] args) {
         Weapon sword = WeaponFactory.getWeapon("sword");
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+
         System.out.println("Sword has " + sword.hit() + " of damage.");
         int armor = 20;
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with " + armor + " points.");
 
         //TODO: Add the remaining weapons here
+
+        System.out.println("Arrow has " + arrow.hit() + " of damage.");
+        armor = 20;
+        System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with " + armor + " points.");
+
 
     }
 }
