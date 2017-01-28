e90c64cbfbf65772358447197feb02bd97c5e9f7
BRawitt00 <brawitt00@gmail.com>
2017-01-27 20:00:12 -0500

    Class TheChosenAxe Added. Added Calculate function to do bulk of work. Changes made to WeaponFactory and RoleGame accordingly.

---
+++ src/edu/fsu/cs/cen4021/armory/TheChosenOneAxe.java
@@ -0,0 +1,70 @@
+package edu.fsu.cs.cen4021.armory;
+import java.io.*;
+
+/**
+ * @author Blake
+ */
+class TheChosenOneAxe extends BasicWeapon implements Weapon {
+
+    private static int x = calculate();
+
+        TheChosenOneAxe()
+        {
+            super(x);
+        }
+
+
+
+@Override
+public int hit()
+        {
+        return DAMAGE;
+        }
+
+@Override
+public int hit(int armor) {
+    int damage;
+    if (armor > 0 && armor < 20)
+        damage = DAMAGE;
+
+    else
+        damage = DAMAGE - armor;
+
+    if (damage < 0)
+        return 0;
+
+    return damage;
+}
+
+
+private static int calculate()
+    {
+        int line_num = 2;
+
+        try{
+            FileInputStream fstream = new FileInputStream("thechosenone.txt");
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String str1 = br.readLine();
+            String str2;
+
+
+            while (true) {
+                str2 = br.readLine();
+                line_num++;
+                if(!str2.equals(str1))
+                {
+                    break;
+                }
+
+            }
+            in.close();
+
+        }
+        catch(Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+        return line_num;
+    }
+
+}
